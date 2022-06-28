7a6d7811255cf5caed94868790c15a88a5795b17
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-06-27 22:20:49 -0600

    Platinum Trophy Unlocked. More choices/conditions

--- WebFun/Week2/Day1/DojoPizzeria/script.js
+++ WebFun/Week2/Day1/DojoPizzeria/script.js
@@ -37,8 +37,8 @@ var cheese = [
     "mozzarella",
     "smoked mozzarella",
     "feta",
-    "vegan cheese",
-    "no cheese"]
+    "scmorza",
+    "burrata"]
 
 var tops = [
     "pineapples",
@@ -54,17 +54,27 @@ var tops = [
     "calabrian chiles"]
 
 function randomPizza(){
-    var sau = Math.floor(Math.random()*5);
-    var cru = Math.floor(Math.random()*5);
-    var che = Math.floor(Math.random()*5);
-    var top = Math.floor(Math.random()*11);
-    var top2 = Math.floor(Math.random()*11);
-    var top3 = Math.floor(Math.random()*11);
-    var allTops = [tops[`${top}`], tops[`${top2}`], tops[`${top3}`]];
-    if (Math.floor(Math.random()*2) == 1) {
+    var sau = Math.floor(Math.random()*5);//Randomly selecting an index of the sauce array
+    var cru = Math.floor(Math.random()*5);//Randomly selecting an index of the crust array
+    var che = Math.floor(Math.random()*5);//Randomly selecting an index of the cheese array
+    if (Math.floor(Math.random()*2) !== che) {//50% chance we'll add another cheese
         var che2 = Math.floor(Math.random()*5);
     }
-    var allChe = [cheese[`${che}`], cheese[`${che2}`]]
+    var allChe = [cheese[`${che}`], cheese[`${che2}`]]//new cheese array for cheese object
+    if (che === che2) {//if the new cheese is the same as the first, then we're keeping just the first
+        allChe = [cheese [`${che}`]];
+    }
+    
+    var top = Math.floor(Math.random()*6);//pick one of the first 6 toppings from toppings array
+    var top2 = Math.floor(Math.random()*11);//top2 declared at random for the array length
+    if ((top2 === top) && (top !== 10)) {//if new random number is the same as the first and is not the 10th index 
+        top2++;//then add 1 to it
+    } else 
+    if (Math.floor(Math.random()*2) !== 1) {// 50-50 chance to add another topping
+        var top3 = Math.floor(Math.random()*11);//pick another topping from the array at random
+    }
+    var allTops = [tops[`${top}`], tops[`${top2}`], tops[`${top3}`]];//new array of arrays for toppings
+    
     var pRand = pizzaOven(crust[`${cru}`], sauce[`${sau}`], [`${allChe}`], [`${allTops}`])
     console.log(pRand);
 }
