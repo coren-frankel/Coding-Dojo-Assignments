0a5bcef9fc56a27640a271271f61439bf77ec15a
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-06-28 20:13:40 -0600

    Platinum Trophy Unlocked!

---
+++ WebFun/Week2/Day2/DojoWeather/script.js
@@ -0,0 +1,37 @@
+function farToCel(far) {
+    var num = (far - 32) * (5 / 9);
+    return Math.round(num * 10) / 10;
+}
+function celToFar(cel) {
+    var num = (cel * (9 / 5)) + 32;
+    return Math.round(num * 10) / 10;
+}
+function convert(option) {
+    console.log(option.value)
+    for(var i = 1; i <= 8; i++){
+        if (option.value == 'farenheit'){
+            var temp = 'temp' + i;
+            next = document.getElementById(`${temp}`).innerText;
+            next = next.slice(0, 2);
+            next = parseInt(next)
+            var converted = Math.round(celToFar(next))
+            var farn = document.querySelector(`#${temp}`)
+            farn.innerText = converted + '°';
+            console.log(converted)
+        }
+        if (option.value == 'celsius'){
+            var temp = 'temp' + i;
+            next = document.getElementById(`${temp}`).innerText;
+            next = next.slice(0, 2);
+            next = parseInt(next)
+            var converted = Math.round(farToCel(next))
+            var id = document.querySelector(`#${temp}`)
+            id.innerText = converted + '°'
+        }
+    }
+    
+}
+function accept() {
+    var cookiesStyle = document.querySelector('#cookies').style;
+    cookiesStyle.display = "none";
+};
