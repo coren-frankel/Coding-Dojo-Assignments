bb24c8c31ff57fc3a81373b17b03e90e7f7f04a3
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-06-30 15:17:18 -0600

    Functionality complete -- for the most part

--- WebFun/Week2/Day4/4FunctionCalculator/script.js
+++ WebFun/Week2/Day4/4FunctionCalculator/script.js
@@ -1,31 +1,57 @@
 var displayDiv = document.querySelector("#display");
 
+function clr(){
+    displayDiv.innerText = 0;
+    operator = '';
+    x = undefined;
+    y = undefined;
+}
+
 function press(num){
     if (displayDiv.innerText == 0){
         displayDiv.innerText = num;
+    } else if (displayDiv.innerText == operator){
+        displayDiv.innerText = num;
+    } else if (displayDiv.innerText > 0){
+        displayDiv.innerText += num;
+    } else if (displayDiv.innerText = '.'){
+        displayDiv.innerText += (0. + num)
     }
-    displayDiv.innerText +=  `${num}`;
-    // console.log(typeof num)
 }
-
+var operator = '';
+var x;
+var y;
+var solution;
 function setOP(math){
-    var last = displayDiv.innerText.slice(-1);
-    // last = Symbol(last);
-    console.log(last)
-    if(last == NaN) {
-        displayDiv.innerText = displayDiv.innerText.splice(-1, 0, last);
-        // last = Symbol(last);
-        displayDiv.innerText += math;
-    } else {
-        // displayDiv.innerText += last;
-        // console.log
+    if (x == undefined){
+        x = displayDiv.innerText.slice(0);
+        displayDiv.innerText = math
+        console.log(x)
+        operator = math;
+        console.log(operator)
     }
-    console.log()
 }
-
-function caculate(temp){
-    var equation = displayDiv.innerText;
-    equation += temp;
-    console.log(equation);
-
+console.log(operator)
+function calculate(){
+    y = displayDiv.innerText.slice(0);
+    console.log(y);
+    console.log(x)
+    console.log(operator)
+    if (operator === '/') {
+        solution = Math.round(x / y);
+    } else if (operator === '*') {
+        solution = x * y;
+    } else if (operator === '-') {
+        solution = x - y;
+    } else if (operator === '+') {
+        solution = x + y;
+    }
+    operator = '';
+    x = undefined;
+    y = undefined;
+    Math.round(solution * 10) / 10;
+    if (solution == Infinity){
+        displayDiv.innerText = '∞'
+    } else 
+        displayDiv.innerText = solution;
 }
