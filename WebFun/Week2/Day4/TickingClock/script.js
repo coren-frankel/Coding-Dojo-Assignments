e597702dd3d9670fad16cffef0d85588b7b800a7
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-06-30 21:39:21 -0600

    Clock Reset - Literal

--- WebFun/Week2/Day4/TickingClock/script.js
+++ WebFun/Week2/Day4/TickingClock/script.js
@@ -1,12 +1,16 @@
 function getSecondsSinceStartOfDay() {
-    return new Date().getSeconds() + 
-      new Date().getMinutes() * 60 + 
-      new Date().getHours() * 3600;
-  }
-      
-  setInterval( function() {
-      var time = getSecondsSinceStartOfDay();
-      console.log(time);
-      // your code here
-  }, 1000);
-  
+    return new Date().getSeconds() +
+        new Date().getMinutes() * 60 +
+        new Date().getHours() * 3600;
+}
+var hour = document.querySelector('hour').style
+var minute = document.querySelector('minutes').style
+var second = document.querySelector('seconds').style
+
+
+
+setInterval(function () {
+    var time = getSecondsSinceStartOfDay();
+    console.log(time);
+    
+}, 1000);
