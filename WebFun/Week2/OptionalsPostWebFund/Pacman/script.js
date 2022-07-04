7bd3e696152e91c6da98956196d6d88de39ded05
unclebabykern <104476731+unclebabykern@users.noreply.github.com>
2022-07-03 23:13:25 -0600

    Random ghost movement; game over function

--- WebFun/Week2/OptionalsPostWebFund/Pacman/script.js
+++ WebFun/Week2/OptionalsPostWebFund/Pacman/script.js
@@ -1,29 +1,45 @@
 function mapChange() {
-    world = [[2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]];
+    world = [[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]];
     for (var i = 1; i < 20; i++) {
         world[i] = [2];//Left wall
         for (var j = 1; j < 20; j++) {
             var rand = Math.ceil(Math.random() * 9);
-            if (rand > 2){
+            if (rand > 2) {
                 rand = 1;
             }
             world[i].push(rand);
         }
         world[i].push(2);//Right wall
     }
-    world[19] = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2];
+    world[19] = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
     world[1][1] = 0;//Pacman's starting position
     world[9][10] = 0;//Blinky's starting position
     world[10][9] = 0;//Pinky's starting position
     world[10][10] = 0;//Inky's starting position
     world[9][9] = 0;//Clyde's starting 
-    
+
     world[9][8] = 2; world[10][8] = 2;//Left ghost wall
     world[8][9] = 2; world[8][10] = 2;//Top ghost wall
     world[9][11] = 2; world[10][11] = 2;//Right ghost wall
     world[11][10] = 2;//ghost funnel
     scoreCheck();
     return world
+}
+function displayWorld() {
+    var output = '';
+    for (var i = 0; i < world.length; i++) {
+        output += "\n<div class='row'>\n";
+        for (var j = 0; j < world[i].length; j++) {
+            if (world[i][j] == 2)
+                output += "<div class='brick'></div>";
+            if (world[i][j] == 1)
+                output += "<div class='coin'></div>";
+            if (world[i][j] == 0)
+                output += "<div class='empty'></div>";
+        }
+        output += "\n</div>";
+    }
+    document.getElementById('world').innerHTML = output;
 }
 var pacman = {
     x: 1,
@@ -46,55 +62,43 @@ var clyde = {
     y: 9
 }
 var total = 0;
-function scoreCheck(){
-    for(var i = 1; i < world.length-1;i++){
-        for(var j = 1; j < world[i].length-1;j++){
-            if(world[i][j] == 1){
+function scoreCheck() {
+    for (var i = 1; i < world.length - 1; i++) {
+        for (var j = 1; j < world[i].length - 1; j++) {
+            if (world[i][j] == 1) {
                 total += 10;
             }
         }
     }
-    console.log(total)
 }
-function endGame(){
-    if (score == total){
+function endGame() {
+    if (score == total) {
         clearInterval(myInt);
-        document.getElementById('GameOver').innterText = 'Congrats, you escaped without any ectoplasm on your... sphere?'
+        clearInterval(blinkyInt);
+        clearInterval(pinkyInt);
+        clearInterval(inkyInt);
+        clearInterval(clydeInt);
+        document.getElementById('gameOver').innerText = 'Wow, you escaped without any ectoplasm on you. Bummer. Also, you win!'
+        
+    } else if (lives == 0){
+        clearInterval(myInt);
+        clearInterval(blinkyInt);
+        clearInterval(pinkyInt);
+        clearInterval(inkyInt);
+        clearInterval(clydeInt);
+        document.getElementById('gameOver').innerText = 'Busted! A ghost stumbled upon to you unhinging your jaw for pizza. Shameful. Boo!'
     }
 }
-
-function displayWorld() {
-    var output = '';
-    for (var i = 0; i < world.length; i++) {
-        output += "\n<div class='row'>\n";
-        for (var j = 0; j < world[i].length; j++) {
-            if (world[i][j] == 2)
-                output += "<div class='brick'></div>";
-            if (world[i][j] == 1)
-                output += "<div class='coin'></div>";
-            if (world[i][j] == 0)
-                output += "<div class='empty'></div>";
-        }
-        output += "\n</div>"
-        // return output;
-    }
-    document.getElementById('world').innerHTML = output;
-}
-mapChange();
-displayWorld();
-displayPacman();
-displayBlinky()
-displayPinky()
-displayInky()
-displayClyde()
-console.log(world);
+mapChange(); displayWorld();
+displayPacman(); displayBlinky(); displayPinky();
+displayInky(); displayClyde();
+var turn = document.getElementById('pacman');
+var score = 0;
+var lives = 3;
+var myInt;
 function displayPacman() {
     document.getElementById('pacman').style.top = pacman.y * 20 + "px";
     document.getElementById('pacman').style.left = pacman.x * 20 + "px";
-    if (world[pacman.y][pacman.x] == world[blinky.y][blinky.x]){
-        
-    }
-
 }
 function displayBlinky() {
     document.getElementById('blinky').style.top = blinky.y * 20 + "px";
@@ -112,56 +116,125 @@ function displayClyde() {
     document.getElementById('clyde').style.top = clyde.y * 20 + "px";
     document.getElementById('clyde').style.left = clyde.x * 20 + "px";
 }
-var turn = document.getElementById('pacman');
-var score = 0; lives = 3;
-var myInt;
-document.onkeydown = function (e) {
+
+var blinkyInt = setInterval(function() { 
+    var move = Math.ceil(Math.random()*4);
+    if ((move == 1) && (world[blinky.y+1][blinky.x] != 2)){
+        blinky.y++;
+        displayBlinky();
+    } else if ((move == 2) && (world[blinky.y-1][blinky.x] != 2)){
+        blinky.y--;
+        displayBlinky();
+    } else if ((move == 3) && (world[blinky.y][blinky.x+1] != 2)){
+        blinky.x++;
+        displayBlinky();
+    } else if ((move == 4) && (world[blinky.y][blinky.x-1] != 2)){
+        blinky.x--;
+        displayBlinky();
+    }
+    
+}, 100);
+var pinkyInt = setInterval(function() { 
+    var move = Math.ceil(Math.random()*4);
+    if ((move == 1) && (world[pinky.y+1][pinky.x] != 2)){
+        pinky.y++;
+        displayPinky();
+    } else if ((move == 2) && (world[pinky.y-1][pinky.x] != 2)){
+        pinky.y--;
+        displayPinky();
+    } else if ((move == 3) && (world[pinky.y][pinky.x+1] != 2)){
+        pinky.x++;
+        displayPinky();
+    } else if ((move == 4) && (world[pinky.y][pinky.x-1] != 2)){
+        pinky.x--;
+        displayPinky();
+    }
+    
+}, 100);
+var inkyInt = setInterval(function() { 
+    var move = Math.ceil(Math.random()*4);
+    if ((move == 1) && (world[inky.y+1][inky.x] != 2)){
+        inky.y++;
+        displayInky();
+    } else if ((move == 2) && (world[inky.y-1][inky.x] != 2)){
+        inky.y--;
+        displayInky();
+    } else if ((move == 3) && (world[inky.y][inky.x+1] != 2)){
+        inky.x++;
+        displayInky();
+    } else if ((move == 4) && (world[inky.y][inky.x-1] != 2)){
+        inky.x--;
+        displayInky();
+    }
+    
+}, 100);
+var clydeInt = setInterval(function() { 
+    var move = Math.ceil(Math.random()*4);
+    if ((move == 1) && (world[clyde.y+1][clyde.x] != 2)){
+        clyde.y++;
+        displayClyde();
+    } else if ((move == 2) && (world[clyde.y-1][clyde.x] != 2)){
+        clyde.y--;
+        displayClyde();
+    } else if ((move == 3) && (world[clyde.y][clyde.x+1] != 2)){
+        clyde.x++;
+        displayClyde();
+    } else if ((move == 4) && (world[clyde.y][clyde.x-1] != 2)){
+        clyde.x--;
+        displayClyde();
+    }
+    
+}, 100);
+
+
+
+
+document.onkeydown = function (e) {//Pacman control
     if (e.keyCode == 40 && world[pacman.y + 1][pacman.x] != 2) {//down
-        clearInterval(myInt);
-        myInt = setInterval(function () {
-            if (world[pacman.y + 1][pacman.x] != 2) {
-                pacman.y++; if ((world[pacman.y][pacman.x]) == 1) {
+        clearInterval(myInt);//halt movement from other keydown
+        myInt = setInterval(function () {//repeat direction movement
+            if (world[pacman.y + 1][pacman.x] != 2) {//until hitting a wall
+                pacman.y++; if ((world[pacman.y][pacman.x]) == 1) {//eat pizza and leave block empty
                     world[pacman.y][pacman.x] = 0;
                     displayWorld();
                     score += 10;
                 }
                 displayPacman();
-                document.getElementById('score').innerText = 'Score:' + score;
+                document.getElementById('score').innerText = 'Score:' + score;//keep score
+
             }
         }, 100);
-        turn.style.transform = 'rotate(90deg)';
+        turn.style.transform = 'rotate(90deg)';//turn to face new direction
     } else if (e.keyCode == 38 && world[pacman.y - 1][pacman.x] != 2) {//up
-        clearInterval(myInt);
+        clearInterval(myInt);//halt movement from other keydown
         myInt = setInterval(function () {
             if (world[pacman.y - 1][pacman.x] != 2) {
-                pacman.y--;turn.style.transform = 'rotate(270deg)'; if ((world[pacman.y][pacman.x]) == 1) {
+                pacman.y--; turn.style.transform = 'rotate(270deg)'; if ((world[pacman.y][pacman.x]) == 1) {
                     world[pacman.y][pacman.x] = 0;
                     displayWorld();
                     score += 10;
                 }
                 displayPacman();
                 document.getElementById('score').innerText = 'Score:' + score;
+
             }
         }, 100);
-        // pacman.y--;
-        ;
     } else if (e.keyCode == 37 && world[pacman.y][pacman.x - 1] != 2) {//left
-        clearInterval(myInt);
+        clearInterval(myInt);//halt movement from other keydown
         myInt = setInterval(function () {
             if (world[pacman.y][pacman.x - 1] != 2) {
-                pacman.x--;turn.style.transform = 'rotate(180deg)'; if ((world[pacman.y][pacman.x]) == 1) {
+                pacman.x--; turn.style.transform = 'rotate(180deg)'; if ((world[pacman.y][pacman.x]) == 1) {
                     world[pacman.y][pacman.x] = 0;
                     displayWorld();
                     score += 10;
                 }
                 displayPacman();
                 document.getElementById('score').innerText = 'Score:' + score;
+
             }
         }, 100);
-        // pacman.x--;
-        
     } else if (e.keyCode == 39 && world[pacman.y][pacman.x + 1] != 2) {//right
-        clearInterval(myInt);
+        clearInterval(myInt);//halt movement from other keydown
         myInt = setInterval(function () {
             if (world[pacman.y][pacman.x + 1] != 2) {
                 pacman.x++; turn.style.transform = 'rotate(0deg)'; if ((world[pacman.y][pacman.x]) == 1) {
@@ -173,8 +246,33 @@ document.onkeydown = function (e) {
                 document.getElementById('score').innerText = 'Score:' + score;
             }
         }, 100);
-        // pacman.x++;
-        
     }
-    endGame();
+    if ((pacman.y === blinky.y) && (pacman.x === blinky.x)) {
+        clearInterval(myInt);
+        pacman.x = 1; pacman.y = 1; --lives;
+        displayPacman();
+        
+        document.getElementById('lives').innerText = "Lives:" + lives;
+    } else if ((pacman.y === pinky.y) && (pacman.x === pinky.x)) {
+        clearInterval(myInt);
+        pacman.x = 1; pacman.y = 1; --lives;
+        displayPacman();
+        
+        document.getElementById('lives').innerText = "Lives:" + lives;
+    } else if ((pacman.y === inky.y) && (pacman.x === inky.x)) {
+        clearInterval(myInt);
+        pacman.x = 1; pacman.y = 1; --lives;
+        displayPacman();
+        
+        document.getElementById('lives').innerText = "Lives:" + lives;
+    } else if ((pacman.y === clyde.y) && (pacman.x === clyde.x)) {
+        clearInterval(myInt);
+        pacman.x = 1; pacman.y = 1; --lives;
+        displayPacman();
+        
+        document.getElementById('lives').innerText = "Lives:" + lives;
+    } else {
+        endGame();
+    }
+    
 }
